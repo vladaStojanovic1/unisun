fbee570bd4589daf6a10c6be4fed976111b587e7
vladaStojanovic1 <1vladastojanovic1@gmail.com>
2021-01-22 11:57:26 +0100

    Section Experience is finished

---
+++ _layouts/_sections/_home/section-experience.php
@@ -0,0 +1,99 @@
+<section class="m-experience">
+    <div class="_wr">
+        <div class="_w m-experience__firstRow">
+            <div class="_xxl7 _xl7 _l7 m-experience__firstRow--text">
+                <h4>
+                    We work with the main institutional investors,
+                    major property owners and general contractors on
+                    the most attractive real estate investments.
+                </h4>
+            </div>
+        </div>
+
+        <div class="_w m-experience__secondRow">
+            <div class="_xxl2 _xl2 _l2 _m6 _s6 m-experience__secondRow--item">
+                <h1>50+</h1>
+                <p>successful transactions</p>
+            </div>
+            <div class="_xxl4 _xl4 _l4 _m6 _s6 m-experience__secondRow--item">
+                <h1>CHF 600 m +</h1>
+                <p>Transaction volume</p>
+            </div>
+            <div class="_xxl3 _xl3 _l3 _m6 _s6 m-experience__secondRow--item">
+                <h1>90 %</h1>
+                <p>Properties sold within 60 days or less</p>
+            </div>
+            <div class="_xxl3 _xl3 _l3 _m6 _s6 m-experience__secondRow--item">
+                <h1>30 +</h1>
+                <p>Active investors</p>
+            </div>
+        </div>
+
+        <div class="_w m-experience__thirdRow">
+            <div class="_xxl3 _xl3 m-experience__thirdRow--left">
+                <h2>Experience the <span>UniSun Invest Difference</span></h2>
+                <p>Buy or sell any investment property tailored to your specific needs and portfolio requirements</p>
+                <a href="#" class="a-btn -blue">Contact Us</a>
+            </div>
+            <div class="_xxl6 _xl6 m-experience__thirdRow--right">
+                <div class="flex">
+                    <div class="card">
+                        <div class="img">
+                            <img src="src/images/_icons/unique-network-icon.svg" alt="">
+                            <p>Unique Network</p>
+                        </div>
+
+                        <div class="text">
+                            <p>
+                                With over 30 active institutional investors and 60 strong partners, we ensure a constant
+                                deal flow and solvent buyers.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="img">
+                            <img src="src/images/_icons/tailored-services-icon.svg" alt="">
+                            <p>Tailored Services</p>
+                        </div>
+
+                        <div class="text">
+                            <p>
+                                We thrive to achieve a 100% match of your search criteria with the properties we offer.
+                                Both on-and offf market deals.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex">
+                    <div class="card">
+                        <div class="img">
+                            <img src="src/images/_icons/unique-network-icon.svg" alt="">
+                            <p>Connecting the dots</p>
+                        </div>
+
+                        <div class="text">
+                            <p>
+                                We provide clear and complete documentation to make your life easier. Always one step
+                                ahead to anticipate tomorrow.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="img">
+                            <img src="src/images/_icons/strong-understanding.svg" alt="">
+                            <p>Strong Understanding</p>
+                        </div>
+
+                        <div class="text">
+                            <p>
+                                Our specialists have comprehensive expertise and know-how of the market. From initial
+                                advice to the conclusion of a contract.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once(dirname(__FILE__) . '/_includes/_head/head_meta.php'); ?>
 <body class="page-<?php echo $page_slug; ?>">
     <div id="page-content" class="page-content">
         <?php require_once(dirname(__FILE__) . '/_includes/_header/header-home.php'); ?>
+        <?php require_once(dirname(__FILE__) . '/_layouts/_sections/_home/section-experience.php'); ?>
         <?php require_once(dirname(__FILE__) . '/_includes/_footer/footer.php'); ?>
     </div>
 </body>
